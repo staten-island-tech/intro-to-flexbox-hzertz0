761541eebe3d918ecb7a18b003a44823081d9f77
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2025-10-07 11:00:55 -0700

    cooked

--- index.js
+++ index.js
@@ -1,2 +1,71 @@
-const card = document.querySelector(".card");
-console.log(btn.nextSibling);
+const blocks = [
+    {name: "Grass Block",
+    amount: 32,
+    image: "Assets/grassblock.png",
+    price: 1},
+
+    {name: "Dirt",
+    amount: 64,
+    image: "Assets/dirt.png",
+    price: 1},
+
+    {name: "Sand",
+    amount: 8,
+    image: "Assets/sand.png",
+    price: 1},
+
+    {name: "Oak Log",
+    amount: 4,
+    image: "Assets/oaklog.png",
+    price: 1},
+
+    {name: "Oak Leaves",
+    amount: 8,
+    image: "Assets/oakleaf.png",
+    price: 1},
+
+    {name: "Stone",
+    amount: 48,
+    image: "Assets/stone.png",
+    price: 1},
+
+    {name: "Cobblestone",
+    amount: 32,
+    image: "Assets/cobblestone.png",
+    price: 1},
+
+    {name: "Deepslate",
+    amount: 48,
+    image: "Assets/deepslate.png",
+    price: 1},
+
+    {name: "Cobbled Deepslate",
+    amount: 64,
+    image: "Assets/cobbledeep.png",
+    price: 1},
+
+    {name: "Netherrack",
+    amount: 64,
+    image: "Assets/netherrack.png",
+    price: 1},
+
+    {name: "Obsidian",
+    amount: 2,
+    image: "Assets/obsidian.png",
+    price: 1},
+
+    {name: "Diamond Block",
+    amount: 1,
+    image: "Assets/diamondblock.png",
+    price: 4},
+
+    {name: "Bookshelf",
+    amount: 1,
+    image: "Assets/bookshelf.png",
+    price: 1},
+
+    {name: "Bedrock",
+    amount: 32,
+    image: "Assets/bedrock.png",
+    price: 10000000000},
+]
