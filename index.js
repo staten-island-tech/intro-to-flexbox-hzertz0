04395290a460e360d42e097cdd5e876c4c738148
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2025-10-16 10:05:18 -0700

    i start count cart total

--- index.js
+++ index.js
@@ -1,18 +1,102 @@
 const blocks = [
-  { name: "Bedrock", amount: 1, image: "Assets/bedrock.png", price: 3.141592653589793, type: "Overworld"},
-  { name: "Bookshelf", amount: 1, image: "Assets/bookshelf.png", price: 1, type: "Craftable" },
-  { name: "Diamond Block", amount: 1, image: "Assets/diamondblock.png", price: 4, type: "Craftable"},
-  { name: "Netherrack", amount: 64, image: "Assets/netherrack.png", price: 1, type: "Nether" },
-  { name: "Obsidian", amount: 2, image: "Assets/obsidian.png", price: 1, type: "Overworld" },
-  { name: "Cobbled Deepslate", amount: 64, image: "Assets/cobbledeep.png", price: 1, type: "Overworld" },
-  { name: "Deepslate", amount: 48, image: "Assets/deepslate.png", price: 1, type: "Overworld" },
-  { name: "Cobblestone", amount: 32, image: "Assets/cobblestone.png", price: 1, type: "Overworld"},
-  { name: "Stone", amount: 48, image: "Assets/stone.png", price: 1, type: "Overworld" },
-  { name: "Oak Leaves", amount: 8, image: "Assets/oakleaf.png", price: 1, type: "Overworld" },
-  { name: "Oak Log", amount: 4, image: "Assets/oaklog.png", price: 1, type: "Overworld" },
-  { name: "Sand", amount: 8, image: "Assets/sand.png", price: 1, type: "Overworld" },
-  { name: "Dirt", amount: 64, image: "Assets/dirt.png", price: 1, type: "Overworld" },
-  { name: "Grass Block", amount: 32, image: "Assets/grassblock.png", price: 1, type: "Overworld" },
+  {
+    name: "Bedrock",
+    amount: 1,
+    image: "Assets/bedrock.png",
+    price: 3.141592653589793,
+    type: "Overworld",
+  },
+  {
+    name: "Bookshelf",
+    amount: 1,
+    image: "Assets/bookshelf.png",
+    price: 1,
+    type: "Craftable",
+  },
+  {
+    name: "Diamond Block",
+    amount: 1,
+    image: "Assets/diamondblock.png",
+    price: 4,
+    type: "Craftable",
+  },
+  {
+    name: "Netherrack",
+    amount: 64,
+    image: "Assets/netherrack.png",
+    price: 1,
+    type: "Nether",
+  },
+  {
+    name: "Obsidian",
+    amount: 2,
+    image: "Assets/obsidian.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Cobbled Deepslate",
+    amount: 64,
+    image: "Assets/cobbledeep.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Deepslate",
+    amount: 48,
+    image: "Assets/deepslate.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Cobblestone",
+    amount: 32,
+    image: "Assets/cobblestone.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Stone",
+    amount: 48,
+    image: "Assets/stone.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Oak Leaves",
+    amount: 8,
+    image: "Assets/oakleaf.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Oak Log",
+    amount: 4,
+    image: "Assets/oaklog.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Sand",
+    amount: 8,
+    image: "Assets/sand.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Dirt",
+    amount: 64,
+    image: "Assets/dirt.png",
+    price: 1,
+    type: "Overworld",
+  },
+  {
+    name: "Grass Block",
+    amount: 32,
+    image: "Assets/grassblock.png",
+    price: 1,
+    type: "Overworld",
+  },
 ];
 
 blocks.forEach((block) => {
@@ -33,15 +117,11 @@ const cart = [];
 document.addEventListener("click", function (click) {
   if (click.target.classList.contains("add-to-cart")) {
     const cartName = click.target.dataset.name;
-    const blockData = blocks.find(b => b.name === cartName);
+    const blockData = blocks.find((b) => b.name === cartName);
 
     if (blockData) {
-      cart.push(blockData)
-      console.log(cart)
+      cart.push(blockData);
+      console.log(blockData.price);
     }
   }
-})
-
-
-
-
+});
